d7d6763a7370461179cfd2f4d29148e278044ff3
Shariph7 <shariphthapa123@gmail.com>
2024-02-16 17:25:34 +0545

    DO_WHILE_LOOP

---
+++ While_Loop/Multiplication.java
@@ -0,0 +1,13 @@
+// WAP TO MAKE A MULTIPLICATION TABLE
+package While_Loop;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int i = 1;
+        int num = 1;
+        while (i<=10) {
+            System.out.println(num+"X"+i+"="+num*i);
+            i++;
+        }
+    }
+}
---
+++ While_Loop/even_n.java
@@ -0,0 +1,12 @@
+// WAP A JAVA PROGRAM TO CALCULATE THE EVEN NUMBER FROM 1 TO 100
+package While_Loop;
+
+public class even_n {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i<=100) {
+            System.out.println(i);
+            i=i+2 ;
+        }
+    }
+}
